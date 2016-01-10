0a8f08fa4758bf40c9c7ac123fcc071d668b71ad
Yuta K <ugwis@icloud.com>
2016-01-10 17:09:14 +0900

    Add member

--- atcoder.py
+++ atcoder.py
@@ -14,7 +14,7 @@ import json
 from requests_oauthlib import OAuth1Session
 from bs4 import BeautifulSoup
 
-checkusers = ['bgpat','goryudyuma','Makinami','murashin','not_seele','toga2048','ugwis','wanimaru47']
+checkusers = ['bgpat','goryudyuma','Makinami','murashin','not_seele','toga2048','ugwis','wanimaru47','tsunetoki']
 
 histogram_filename = "hist-atcoder.png"
 
