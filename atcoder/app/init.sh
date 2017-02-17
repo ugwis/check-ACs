1675ffccde5a293f4f15bd5c9f33095f00dd5152
Kitamura Yuta <ugwis@icloud.com>
2017-02-18 05:25:20 +0900

    取得漏れがないように再試行

--- atcoder/app/init.sh
+++ atcoder/app/init.sh
@@ -5,6 +5,11 @@ psql -U postgres -h pgsql -f create_database.sql atcoderdb
 python contest_crawler.py
 python solved_crawler.py
 psql -U postgres -c 'UPDATE solved SET checked=True;' atcoderdb
+
+python contest_crawler.py
+python solved_crawler.py
+psql -U postgres -c 'UPDATE solved SET checked=True;' atcoderdb
+
 psql -U postgres -h pgsql -f create_registrants.sql atcoderdb
 
 sh crawl.sh
